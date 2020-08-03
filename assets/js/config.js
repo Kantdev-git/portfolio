ca52a55b9ac95f99222009aeaf6100e489f8c7d8
I.K <xix@mbp-de-ik.home>
2020-08-03 02:51:50 +0200

    V finale

---
+++ assets/js/config.js
@@ -0,0 +1,1 @@
+var firebaseConfig={apiKey:"AIzaSyCxeS826-1SJda89rps0RvMZWMJ-amBQGM",authDomain:"portfolio-7775e.firebaseapp.com",databaseURL:"https://portfolio-7775e.firebaseio.com",projectId:"portfolio-7775e",storageBucket:"portfolio-7775e.appspot.com",messagingSenderId:"265653563497",appId:"1:265653563497:web:c611eac186b18bb17b72f8"};firebase.initializeApp(firebaseConfig);
